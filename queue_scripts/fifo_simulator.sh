55dcd34825a686bb54c4ee954a74698b8d174f46
jpinn97 <jp878@canterbury.ac.uk>
2024-03-20 12:22:14 +0000

    ammend

--- queue_scripts/fifo_simulator.sh
+++ queue_scripts/fifo_simulator.sh
@@ -13,11 +13,11 @@ if [ "$REPLY" = "y" ]; then
     if [ -z "$input" ]; then
         echo "No pre-defined input found"
         read -r -p "Enter 10 Byte tasks (comma seperated): " input
-        echo "$input" >./data/users/simdata_"$1".job
+        echo "$input" | sudo tee ./data/users/simdata_"$1".job
     fi
 else
     read -r -p "Enter 10 Byte tasks (comma seperated): " input
-    echo "$input" >./data/users/simdata_"$1".job
+    echo "$input" | sudo tee ./data/users/simdata_"$1".job
 fi
 
 queue=""
